f1463ed0e11ca2b1769068c4b2d001a61154706b
Matthew Storti <stortim@sheridancollege.ca>
2026-01-27 16:45:19 -0500

    Project set up and basic UI (index.html) completed

---
+++ src/test/java/sheridan/stortim/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.stortim.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
