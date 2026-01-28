22b7dde38b813b3ecdabce58b6b415212b1a3e3b
Matthew Storti <stortim@sheridancollege.ca>
2026-01-28 14:11:06 -0500

    Validation dependency added, and output HTML file created

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
